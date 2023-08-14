d7fc8d9643377fd67ef8df20fc4f3275317e9869
Raftouch <rafanelkin@gmail.com>
2023-08-14 16:11:01 +0200

    :bug: fixed bug with cors

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,18 +1,16 @@
 import express, { Request, Response } from 'express'
 import { sampleProducts } from './data'
-import corsMiddleware from './middleware/cors'
-// import cors from 'cors'
+import cors from 'cors'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
 const app = express()
-app.use(corsMiddleware)
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: ['http://localhost:5173'],
-//   })
-// )
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:5173'],
+  })
+)
 
 app.get('/api/products', (req: Request, res: Response) => {
   res.json(sampleProducts)
